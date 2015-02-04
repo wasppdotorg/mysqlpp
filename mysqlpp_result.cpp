7ad469322778f5e7d5e8ea1cb8cdd13edb4a87b5
WASPP <waspp.org@gmail.com>
2015-02-04 18:36:39 +0900

    result still working on

--- mysqlpp_result.cpp
+++ mysqlpp_result.cpp
@@ -5,6 +5,7 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
+#include <vector>
 #include <stdexcept>
 
 #include <mysql/mysql.h>
@@ -15,8 +16,10 @@
 namespace mysqlpp
 {
 
-result::result(st_mysql_stmt* stmt)
+result::result(st_mysql_stmt* stmt) : stmt(0), current_row(0), metadata(0)
 {
+	iss.imbue(std::locale::classic());
+
 	if (mysql_stmt_store_result(stmt) == 0)
 	{
 		throw exception(mysql_stmt_error(stmt));
@@ -36,6 +39,78 @@ result::~result()
 	if (metadata)
 	{
 		mysql_free_result(metadata);
+	}
+}
+
+/*
+bool result::next()
+{
+	++current_row;
+	reset_field();
+
+	if (data_index > 0)
+	{
+		if (myssql_stmt_bind_result(stmt, &binds[0]))
+		{
+			throw exception(mysql_stmt_error(stmt));
+		}
+	}
+}
+*/
+
+std::string result:: name(int index)
+{
+	if (index < 0 || index > field_count)
+	{
+		throw exception("invalid field_index");
+	}
+
+	st_mysql_field* fields = mysql_fetch_fields(metadata);
+	if (!fields)
+	{
+		throw exception("empty fields");
+	}
+
+	return fields[index].name;
+}
+
+int result::index(const std::string& name)
+{
+	st_mysql_field* fields = mysql_fetch_fields(metadata);
+	if (!fields)
+	{
+		throw exception("empty fields");
+	}
+
+	for (std::size_t i = 0; i < field_count; ++i)
+	{
+		if (name == fields[i].name)
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+void result::reset()
+{
+	binds.resize(0);
+	binds.resize(field_count, st_mysql_bind());
+
+	bind_data.resize(0);
+	bind_data.resize(field_count, st_mysql_bind_data());
+
+	for (std::size_t i = 0; i < field_count; ++i)
+	{
+		binds[i].buffer_type = MYSQL_TYPE_STRING;
+		binds[i].buffer = bind_data[i].buf;
+		binds[i].buffer_length = sizeof(bind_data[i].buf);
+		binds[i].length = &bind_data[i].length;
+		binds[i].is_null = &bind_data[i].is_null;
+		binds[i].error = &bind_data[i].error;
+		
+		bind_data[i].ptr = bind_data[i].buf;
 	}
 }
 
--- mysqlpp_result.hpp
+++ mysqlpp_result.hpp
@@ -8,6 +8,10 @@
 #ifndef MYSQLPP_RESULT_HPP
 #define MYSQLPP_RESULT_HPP
 
+#include <string>
+#include <vector>
+#include <sstream>
+
 #include <mysql/mysql.h>
 
 #include "mysqlpp_result.hpp"
@@ -15,17 +19,69 @@
 namespace mysqlpp
 {
 
+struct st_mysql_bind_data
+{
+	st_mysql_bind_data()
+	{
+
+	}
+
+	char buf[128];
+	std::vector<char> vbuf;
+	char* ptr;
+
+	unsigned long length;
+
+	my_bool is_null;
+	my_bool error;
+};
+
+
 class result
 {
 public:
 	result(st_mysql_stmt* stmt);
 	~result();
 
+	void reset();
+	std::string name(int field_index);
+	int index(const std::string& name);
+
 private:
+	/*template<typename T>
+		bool do_fetch(int col,T &v)
+		{
+		bind_data &d=at(col);
+		if(d.is_null)
+		return false;
+
+		v=parse_number<T>(std::string(d.ptr,d.length),fmt_);
+		return true;
+		}*/
+
+	template<typename T> bool fetch(T& value)
+	{
+		st_mysql_bind_data &data = bind_data[field_index];
+		if (data.is_null)
+		{
+			return false;
+		}
+
+
+
+	}
+
+	st_mysql_stmt* stmt;
+	st_mysql_res* metadata;
+	
+	int field_index;
 	unsigned int field_count;
 	unsigned int current_row;
+	
+	std::vector<st_mysql_bind> binds;
+	std::vector<st_mysql_bind_data> bind_data;
 
-	st_mysql_res* metadata;
+	std::istringstream iss;
 };
 
 } // namespace mysqlpp
