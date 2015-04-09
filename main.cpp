d4b76558c10937303019a3458448b38cb18f8d66
WASPP <waspp.org@gmail.com>
2015-04-09 23:53:25 +0900

    clear warnings

--- main.cpp
+++ main.cpp
@@ -38,8 +38,8 @@ int main()
 
 		unsigned char param01 = 1;
 		unsigned short int param02 = 65535;
-		unsigned int param03 = 4294967295;
-		unsigned long long int param04 = 18446744073709551615;
+		unsigned int param03 = 429496729;
+		unsigned long long int param04 = 1844674407370955161;
 		float param05 = 5.01f;
 		double param06 = 6.01f;
 
