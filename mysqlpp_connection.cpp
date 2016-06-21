7e4c3e83e2bd8b210dd14e4ff321f12c10f9c4be
MobileGameServer <mobilegameserver@gmail.com>
2016-06-21 20:26:42 +0900

    error with vs2013

--- mysqlpp_connection.cpp
+++ mysqlpp_connection.cpp
@@ -61,7 +61,7 @@ namespace mysqlpp
 
 	statement* connection::prepare_like(const std::string& query, bool left_percent, const std::string& keyword, bool right_percent)
 	{
-		char escaped_keyword[(keyword.size() * 2) + 1] = { 0 };
+		auto escaped_keyword = new char[(keyword.size() * 2) + 1];
 
 		mysql_real_escape_string(mysql, escaped_keyword, keyword.c_str(), keyword.size());
 
@@ -73,6 +73,7 @@ namespace mysqlpp
 			oss << "%";
 		}
 		oss << escaped_keyword;
+		delete[] escaped_keyword;
 
 		if (right_percent)
 		{
